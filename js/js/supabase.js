c3376d8f39b3c94f0c9e81ee67802f9c6c84a6e9
crippsclara <crippsclar@gmail.com>
2026-07-14 15:36:15 -0400

    Delete js/js/supabase.js

--- js/js/supabase.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://kbznbtpupgxuogluourw.supabase.co/rest/v1/";
-const SUPABASE_KEY = "sb_publishable_XwU9BOyUUe6fTNYWVVZf3Q_F3jti8i8";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
